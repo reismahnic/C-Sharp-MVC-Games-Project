28fe0cd7e87b0e7c33a8d4d8980ac64748413a37
reismahnic <reismahnic@gmail.com>
2017-11-10 18:50:02 -0800

    Initial Commit

---
+++ Games Project/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Games_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Games_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var videoGames = VideoGameManager.GetVideoGames();
+            return View(videoGames);
+        }
+    }
+}
---
+++ Games Project/Models/VideoGameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Games_Project.Models
+{
+    public class VideoGameManager
+    {
+        public static List<VideoGame> GetVideoGames()
+        {
+            return new List<VideoGame>
+            {
+                new VideoGame {VideoGameID=1, GameTitle="Nier", Developer="Cavia", ReleaseYear=2010, Characters= new List<Character> { new Character { CharacterID = 1, Name="Nier"}, new Character { CharacterID = 2, Name = "Kaine" }, new Character { CharacterID = 3, Name = "Emil" } } },
+                new VideoGame {VideoGameID=2, GameTitle="Night in the Woods", Developer="Infinite Fall", ReleaseYear=2017, Characters= new List<Character> { new Character { CharacterID = 1, Name="Mae Borowski"}, new Character { CharacterID = 2, Name = "Gregg Lee" }, new Character { CharacterID = 3, Name = "Bea Santello" } } },
+                new VideoGame {VideoGameID=3, GameTitle="Grim Fandango", Developer="Lucasarts", ReleaseYear=1998, Characters= new List<Character> { new Character { CharacterID = 1, Name="Manny Calavera"}, new Character { CharacterID = 2, Name = "Meche Colomar" }, new Character { CharacterID = 3, Name = "Glottis" } } }
+            };
+        }
+    }
+    public class VideoGame
+    {
+        public int VideoGameID { get; set; }
+        public string GameTitle { get; set; }
+        public string Developer { get; set; }
+        public int ReleaseYear { get; set; }
+        public List<Character> Characters { get; set; }
+    }
+          
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        public string Name { get; set; }
+    }
+}
